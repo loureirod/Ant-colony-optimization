5639bf38c5991bb626f5e88788fda83dd0a228fd
Zhof <teen95@hotmail.fr>
2018-04-13 00:44:32 +0200

    origin

--- graphique.py
+++ graphique.py
@@ -7,6 +7,7 @@ import random
 import time
 from random import randint
 from math import sqrt
+import tkinter.font as tkfont
 
 Nods=[]
 Distance=[]
@@ -14,7 +15,7 @@ first=True
 last=False
 HEIGHT=400
 WIDTH=800
-N_ants=5
+
 
 root=Tk()                
 canvas=Canvas(root,height=HEIGHT, width=WIDTH,bg='white') ## création du canvas
@@ -67,74 +68,75 @@ root.quit()
 
 N=len(Nods)
 Speed=3
-
 Graph=np.zeros((N,N))
 for i in Distance:
-    Graph[i[0]][i[1]]=int(i[2])
-    Graph[i[1]][i[0]]=int(i[2])
+    Graph[i[0]][i[1]]=int(sqrt(i[2]))
+    Graph[i[1]][i[0]]=int(sqrt(i[2]))
 print(Graph)
 
-dx=20
 fenetre = Tk()
 can = Canvas(fenetre,bg='dark grey',height=HEIGHT, width=WIDTH)
 can.pack(side=LEFT,padx=5,pady=5)
+bold_font = tkfont.Font(family="Helvetica", size=12, weight="bold")
 
-rond=can.create_oval(Nods[0][0]-10,Nods[0][1]-10,(Nods[0][0]+10),(Nods[0][1]+10),fill="red",width=2)
+rond=can.create_oval(Nods[0][0]-12,Nods[0][1]-12,(Nods[0][0]+12),(Nods[0][1]+12),fill="red",width=2)
+can.create_text(Nods[0][0]-15, Nods[0][1]-15,text='0',fill="purple",font=bold_font)
 for i in range(1,len(Nods)-1):
-    rond=can.create_oval(Nods[i][0]-10,Nods[i][1]-10,(Nods[i][0]+10),(Nods[i][1]+10),fill="grey",width=2)
-rond=can.create_oval(Nods[N-1][0]-10,Nods[N-1][1]-10,(Nods[N-1][0]+10),(Nods[N-1][1]+10),fill="green",width=2)
+    rond=can.create_oval(Nods[i][0]-12,Nods[i][1]-12,(Nods[i][0]+12),(Nods[i][1]+12),fill="grey",width=2)
+    can.create_text(Nods[i][0]-15, Nods[i][1]-15,text='%s'%i,fill="purple",font=bold_font)
+rond=can.create_oval(Nods[N-1][0]-12,Nods[N-1][1]-12,(Nods[N-1][0]+12),(Nods[N-1][1]+12),fill="green",width=2)
+can.create_text(Nods[-1][0]-15, Nods[-1][1]-15,text='%s'%(len(Nods)-1),fill="purple",font=bold_font)
 for i in range(len(Distance)):
     trait=can.create_line(Nods[Distance[i][0]][0], Nods[Distance[i][0]][1], Nods[Distance[i][1]][0], Nods[Distance[i][1]][1],dash=((4,4)),width=3)
 
-number_ants = 20
+number_ants = 1
 evaporation_rate = 0.00005
 steps = 1000
 environment = environment.Environment(Graph,number_ants,evaporation_rate)
-active=True
+active=False
 
 def anime():
     environment.step()
     for i in range(0,number_ants):
         afficher(environment.population[i])
-    fenetre.after(1,anime)
+    if active==True:
+    	fenetre.after(5,anime)
 
     
 def start():
-    global flag
-    flag=1
-    for i in range(0,number_ants):
-    	environment.population[i].x=Nods[environment.population[i].road[0]][0]
-    	environment.population[i].y=Nods[environment.population[i].road[0]][1]
-    	environment.population[i].oval=can.create_oval(environment.population[i].x-5,environment.population[i].y-5,environment.population[i].x+5,environment.population[i].y+5,width=2,fill="blue") #draw the initial corps of the ant
-    anime()	
+	global active
+	active=True
+	if hasattr(environment.population[0],'oval'): #hasattr() allows us to know if there is an oval attribute in population, so if start has aready been clicked one time
+		pass
+	else:
+		for i in range(0,number_ants):
+			environment.population[i].x=Nods[environment.population[i].road[0]][0]
+			environment.population[i].y=Nods[environment.population[i].road[0]][1]
+			environment.population[i].oval=can.create_oval(environment.population[i].x-5,environment.population[i].y-5,environment.population[i].x+5,environment.population[i].y+5,width=2,fill="blue") #draw the initial corps of the ant
+	anime()	
 
 def stop():
-    global flag
-    flag=0
-    for i in range(0,number_ants):
-        environment.population[i].stop()
-        self.x= Nods[self.road[0]][0]
-        self.y= Nods[self.road[0]][1]
-        self.oval=can.create_oval(self.x-5,self.y-5,self.x+5,self.y+5,width=2,fill="blue") #draw the initial corps of the ant
+	global active
+	active=False
+	print('Pheromone levels:')
+	print(np.around(environment.pheromone,2))
+	print('Best path:')
+	print(environment.best_path())
 
 
 def afficher(Anti):
-	Anti.x=Nods[Anti.road[0]][0] + Anti.road_step/Graph[Anti.road[0]][Anti.road[1]]*(Nods[Anti.road[1]][0]-Nods[Anti.road[0]][0])
-	Anti.y=Nods[Anti.road[0]][1] + Anti.road_step/Graph[Anti.road[0]][Anti.road[1]]*(Nods[Anti.road[1]][1]-Nods[Anti.road[0]][1])
+	Anti.x=Nods[Anti.road[0]][0] + Anti.road_step/(Graph[Anti.road[0]][Anti.road[1]]+1)*(Nods[Anti.road[1]][0]-Nods[Anti.road[0]][0])
+	Anti.y=Nods[Anti.road[0]][1] + Anti.road_step/(Graph[Anti.road[0]][Anti.road[1]]+1)*(Nods[Anti.road[1]][1]-Nods[Anti.road[0]][1])
+	
 	can.coords(Anti.oval,Anti.x-5,Anti.y-5,Anti.x+5,Anti.y+5)
 
 
 
-boutonGO = Button(fenetre, text='GO', width =3,command=start)
+
+boutonGO = Button(fenetre, text='GO', width =20,command=start)
 boutonGO.pack(side=BOTTOM)
-boutonSTOP = Button(fenetre, text='STOP', width =3,command=stop)
+boutonSTOP = Button(fenetre, text='STOP', width =20,command=stop)
 boutonSTOP.pack(side=BOTTOM)
      
 fenetre.mainloop()
 
-print('Pheromone levels:')
-print(np.around(environment.pheromone,2))
-print('Best path:')
-print(environment.best_path())
-
-print("--Code executed--")
